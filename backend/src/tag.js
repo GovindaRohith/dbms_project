4fe3a07ebfd8d183f5e8b809ac50f4a1cc0e0885
rohith <GovindaRohith>
2023-02-15 16:04:20 +0530

    Tag_Back_Done

--- backend/src/tag.js
+++ backend/src/tag.js
@@ -1,13 +1,41 @@
 const express = require('express')
 const router = express.Router()
 const path = require('path')
+const conn=require('./db')
 
 router.post('/', (req, res)=>
 {
     //TODO: send questions for given list of tags
-    var tags=[req.body]
-    //tags: ["str1","str2","str3"]
-    var str=""
+    var rec="";
+    for(i=0;i<req.body.length;i++)
+    {
+        rec=rec+req.body[i]+" ";
+    }
+    console.log(rec)
+    rec=rec.trim()
+    var tags=rec.split(" ");
+    console.log(tags)
+    var str=" (B.tag_name='"+tags[0]+"' ";
+    //or B.tag_name='C' //end here
+    for(i=1;i<tags.length;i++)
+    {
+        str=str+"or B.tag_name='"+tags[i]+"' "
+    }
+    str=str+") )"
+    var pre_query="with posts_tag (post_id) as "+
+   " ( "+
+   "select A.post_id as post_id "+
+   "FROM tag_posts as A,tags as B "+
+   "WHERE A.tag_id=B.tag_id and ";
+    var pos_query="select E.post_id,E.owner_display_name,E.last_editor_display_name,E.last_edit_date,E.up_vote,E.down_vote,E.score,E.acc_ans_count,E.post_title,E.content_license,E.body_text,E.creation_date,E.closed_date"+
+    " from posts as E,posts_tag as C "+
+    " WHERE E.post_id=C.post_id and post_type_id=1; ";
+    // console.log(pre_query+str+pos_query);
+    conn.query(pre_query+str+pos_query, (err, rows, fields) => {
+        if (err) throw err
+        res.send(rows)
+      })
+    //get a string and seperate into arrays and do query on words
 })
 
 module.exports = router
